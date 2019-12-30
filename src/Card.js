02a155bb5dd4cb95e341a1bae5254a663a0255e2
JTLiner925 <[jtliner925@gmail.com]>
2019-12-30 14:03:29 -0600

    adding jxl

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+
+function Card(title,content) {
+  return (
+    <div class='Card'>
+        <h3>{allCards.title}</h3>
+        <p>{allCards.content}</p>
+    </div>
+);
+}
+
+export default Card;
